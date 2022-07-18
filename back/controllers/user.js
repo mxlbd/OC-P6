09beae48b25ba1a1d144a44785b7f21b2ee93560
Maxime Leblond <maxime.lbd@outlook.fr>
2022-07-18 19:31:24 +0200

    initialisation queries sauce/ECMAScript2017 signup

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -4,33 +4,31 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cryptoJs = require('crypto-js');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res) => {
   const { email, password } = req.body;
 
   const emailCryptoJs = cryptoJs
-    .HmacSHA256(email, process.env.EMAIL_SECRET)
+    .HmacSHA256(email, process.env.CRYPTOJS)
     .toString();
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: emailCryptoJs,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  try {
+    const hash = await bcrypt.hash(password, 10);
+    const user = new User({
+      email: emailCryptoJs,
+      password: hash,
+    });
+    user.save();
+    res.status(201).json({ message: 'Utilisateur créé !' });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
-exports.login = (req, res, next) => {
+exports.login = (req, res) => {
   const { email, password } = req.body;
 
   const emailCryptoJs = cryptoJs
-    .HmacSHA256(email, process.env.EMAIL_SECRET)
+    .HmacSHA256(email, process.env.CRYPTOJS)
     .toString();
 
   User.findOne({ email: emailCryptoJs })
@@ -48,13 +46,9 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: jwt.sign(
-                  { userId: user._id },
-                  process.env.TOKEN_SECRET,
-                  {
-                    expiresIn: '24h',
-                  }
-                ),
+                token: jwt.sign({ userId: user._id }, process.env.JWT, {
+                  expiresIn: '24h',
+                }),
               });
             }
           })
